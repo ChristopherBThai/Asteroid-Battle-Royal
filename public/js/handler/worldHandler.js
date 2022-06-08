30a3d982e7c26cda5d6a59fc71bd2f8ff609c632
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-08 13:47:03 +0800

    Delete public directory

--- public/js/handler/worldHandler.js
+++
@@ -1,7 +0,0 @@
-const worldHeight = 4000;
-const worldWidth = 4000;
-
-function createWorld(self){
-	self.physics.world.bounds.width = worldWidth;
-	self.physics.world.bounds.height = worldHeight;
-}
